cd4873fcc825916daa60b7bcaeae71ceea48c8bd
Ashish Neupane <aasesshenry@gmail.com>
2020-09-02 23:10:49 +0545

    neccessary basic things done!!!

--- ProductBundleRecommendation/settings.py
+++ ProductBundleRecommendation/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Recommendation'
 ]
 
 MIDDLEWARE = [
@@ -117,4 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'statics'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR/ 'ProductBundleRecommendation/static'
+]
+
